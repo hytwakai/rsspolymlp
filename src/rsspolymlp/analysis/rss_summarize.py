2358de284a5ac567ed556514f30e2d4ef6f37dc2
hytwakai <hayato101877@outlook.jp>
2025-09-26 07:48:52 +0000

    Implementing a simpler method to obtain reduced crystal structure representations

--- src/rsspolymlp/analysis/rss_summarize.py
+++ src/rsspolymlp/analysis/rss_summarize.py
@@ -306,13 +306,22 @@ class RSSResultSummarizer:
             for res in rss_results:
                 if not res.get("is_ghost_minima") and e_min is None:
                     e_min = res["energy"]
+                    ghost_num = res["struct_no"]
+                    for num in range(ghost_num):
+                        for _res in rss_results:
+                            if num + 1 == _res["struct_no"]:
+                                dest = (
+                                    f"{dir_name}/{logname}/POSCAR_{logname}_No{num+1}"
+                                )
+                                if output_poscar:
+                                    shutil.copy(_res["poscar"], dest)
+                                break
+
                 if e_min is not None and threshold is not None:
                     diff = abs(e_min - res["energy"])
                     if diff * 1000 > threshold:
-                        if struct_count == 1:
-                            print("The threshold value is probably too small.")
-                            e_min = None
                         continue
+
                 dest = f"{dir_name}/{logname}/POSCAR_{logname}_No{res['struct_no']}"
                 if output_poscar:
                     shutil.copy(res["poscar"], dest)
