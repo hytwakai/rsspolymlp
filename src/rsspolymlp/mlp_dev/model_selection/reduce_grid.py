7b1d7005f25d5d40047104620f8ac53cf372847c
hytwakai <hayato101877@outlook.jp>
2026-03-30 04:58:00 +0000

    Add single MLP model candidates for binary systems

--- src/rsspolymlp/mlp_dev/model_selection/reduce_grid.py
+++ src/rsspolymlp/mlp_dev/model_selection/reduce_grid.py
@@ -10,6 +10,12 @@ parser.add_argument(
     default=None,
     help="Directory path including model parameter candidates",
 )
+parser.add_argument(
+    "--max_n",
+    type=int,
+    default=80000,
+    help="",
+)
 args = parser.parse_args()
 
 grid_path = args.path
@@ -19,7 +25,7 @@ count = 0
 for path in path_all:
     with open(f"{path}/n_feature.dat", "r") as f:
         n_feature = int([i.strip() for i in f][0])
-    if n_feature < 80000:
+    if n_feature < args.max_n:
         count += 1
         os.makedirs(f"{grid_path}_reduce", exist_ok=True)
         shutil.copytree(f"{path}", f"{grid_path}_reduce/polymlp-" + str(count).zfill(4))
